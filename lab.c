233ff69d40bf52e705415c7c022196ac00288ba0
VoyakaGOD <ab3000645@gmail.com>
2022-12-09 13:53:00 +0300

    mayne

--- lab.c
+++ lab.c
@@ -12,8 +12,15 @@ int main()
     double int2 = 0;
     fscanf(file, "%lf%lf%lf%lf%lf%lf%lf", &start_time, &end_time, &step, &precision, &const_betta, &int1, &int2);
 
+    double *radioactivity = (double *)calloc((int)(2 * (end_time - start_time) / step));
+    double *time = (double *)calloc((int)(2 * (end_time - start_time) / step));
+    int N = experiment(radioactivity, time, start_time, end_time, step);
+    add_noise(radioactivity, N);
+    double decay_time = nonlinear_equation(radioactivity, time, N, precision, int1, int2);
+    double decate_rate = linear_equation(radioactivity, time, N);
+    double time_differs = precision_analysis(radioactivity, time, N);
 
-    //int N = experiment(double *radioactivity, double *time, start_time, end_time, step);
+    printf("%lf, %lf, %lf\n", decay_time, decate_rate, time_differs);
 
     fclose(file);
 }
