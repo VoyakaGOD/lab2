2db36309e4e224f1405fd93a82ddf9b2664a34d9
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2022-12-09 14:07:36 +0300

    experiment and noise

---
+++ experiment.c
@@ -0,0 +1,23 @@
+// #include <math.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+
+int experiment (double *radioactivity, double *time, double start_time, double end_time, double step) 
+{
+    int N = 1;
+    double beta = 0;
+    printf ("\nEnter beta-constant: ");
+    scanf ("%lf", beta);
+    time[0] = start_time;
+    radioactivity[0] = pow (exp(1), -time[0] / beta);
+
+    for (int i = 1; i <= 100 && time[i - 1] <= end_time; i++)
+    {
+        time[i] = time[i-1] + step;
+        radioactivity[i] = pow (exp(1), -time[i] / beta);
+        N++;
+    }
+
+    return N;
+}
