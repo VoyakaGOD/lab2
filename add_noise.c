138c86f7a4d8bd668eb375bce9cfe83d7bd2d11f
BoredLudleth <111277087+BoredLudleth@users.noreply.github.com>
2022-12-09 14:33:04 +0300

    Update add_noise.c

--- add_noise.c
+++ add_noise.c
@@ -6,10 +6,10 @@
 void add_noise (double *radioactivity, int N)
 {
     srand (time (NULL));
-    double random_value = (double)rand()/RAND_MAX*2.0-1.0;
 
     for (int i = 0; i < N; i++)
     {
+        double random_value = (double)rand()/RAND_MAX*2.0-1.0;
         radioactivity[i] = radioactivity[i] + random_value * 0.05;
     }
 }
